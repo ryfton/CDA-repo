9281cd970d0694608489cf63d85ad88ade36a6b0
tarik <73602347+ryfton@users.noreply.github.com>
2021-05-21 11:08:31 +0200

    vendredi

--- JeuDu0-1-2/Program.cs
+++ JeuDu0-1-2/Program.cs
@@ -10,54 +10,75 @@ namespace JeuDu0_1_2
     {
         static void Main(string[] args)
         {
-            int scorePC=0;
-            int scoreJoueur=0;
-            int nombreJoueur=0;
-            var difference=0;
+            int scorePC = 0;
+            int scoreJoueur = 0;
+            int nombreJoueur = 0;
+            var difference = 0;
             int nombreMachine;
             //int[] nombreMachine=new int[3] { 0, 1,2 };
             Random aleatoire = new Random();
+            bool partiAnulé = false;
 
 
 
-            while (scoreJoueur != 10 && scorePC != 10) 
+
+
+
+            while (scoreJoueur != 10 && scorePC != 10 && partiAnulé != true)
             {
-                nombreMachine=aleatoire.Next(0, 2);
+
+
+                nombreMachine = aleatoire.Next(0, 2);
                 Console.WriteLine("Veuillez choisir un chiffre entre 0 et 2 compris");
                 nombreJoueur = int.Parse(Console.ReadLine());
-                
-                if (nombreJoueur<nombreMachine)
+                if (nombreJoueur < 0)
                 {
-                    difference=nombreMachine - nombreJoueur;
-                    if (difference==1)
-                    {
-                        scoreJoueur++;
-                    }
-                    else
-                    {
-                        scorePC++;
-                    }
+                    Console.WriteLine("La partie est fini");
+                    partiAnulé = true;
                 }
-                else if (nombreJoueur > nombreMachine)
+                else
                 {
-                    difference = nombreJoueur- nombreMachine;
-                    if (difference == 1)
+
+
+                    if (nombreJoueur < nombreMachine)
                     {
-                        scorePC++;
+                        difference = nombreMachine - nombreJoueur;
+                        if (difference == 1)
+                        {
+                            scoreJoueur++;
+                        }
+                        else
+                        {
+                            scorePC++;
+                        }
                     }
-                    else
+                    else if (nombreJoueur > nombreMachine)
                     {
-                        scoreJoueur++;
+                        difference = nombreJoueur - nombreMachine;
+                        if (difference == 1)
+                        {
+                            scorePC++;
+                        }
+                        else
+                        {
+                            scoreJoueur++;
+                        }
                     }
+
+                    Console.WriteLine("le score est de [" + scoreJoueur + "] pour le joueur et de [" + scorePC + "] pour l' IA.");
+                    Console.WriteLine();
+
+
                 }
-
-                Console.WriteLine("le score est de [" + scoreJoueur + "] pour le joueur et de [" + scorePC+ "] pour l' IA.");
-                Console.WriteLine();
+            }
 
 
-            } 
 
-            if (scoreJoueur<scorePC)
+            if (partiAnulé)
+            {
+                Console.WriteLine("Le joueur d'éclare forfait");
+            }
+            else if (scoreJoueur < scorePC)
             {
                 Console.WriteLine("Le grand gagant est l'IA ");
             }
--- TriDunTableau/Program.cs
+++ TriDunTableau/Program.cs
@@ -4,64 +4,43 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TriDunTableau
+namespace Palindrom
+
 {
     class Program
     {
         static void Main(string[] args)
-        {   
-            List<int> nombre = new List<int>();
-            Console.WriteLine("Ce programme permet de trier les éléments d'un tableau :");
+        {
+            string phrase,phraseReverse,finDePhrase;
+            bool palindrom=false;
+            
+            Console.WriteLine("veuillez saisir une phrase terminer par un point:");
+            phrase = Console.ReadLine().ToLower();
+            
+            // teste de phrase vide.
+            finDePhrase=phrase.Substring(phrase.Length - 1);
+            if (finDePhrase == "."&& phrase.Length<1)
+            {
+                Console.WriteLine("la phrase est vide");
+            }else
+            { phrase = phrase.Substring(0,phrase.Length-1);
+                while (!palindrom)
+                {
 
-            Console.WriteLine("Veuillez saisir un nombre :");
+                    Console.WriteLine(phrase);
+
+                }
+
+
+            }
+
+
+            Console.ReadLine();
+
+
+        }
             
 
 
-            for ( int i=0 ; i < nombre.Count; i++)
-            {
-                
-                nombre[i] = Convert.ToInt16(Console.ReadLine());
-            }
-
-            TriBule(nombre);
-
-
-
-            for ( int i = 0; i < nombre.Count; i++)
-            {
-                Console.Write(nombre[i] + "- \t");
-            }
-            //Console.WriteLine();
-
-            //Console.WriteLine("tapez une touche pour quitter");
-            Console.ReadKey();
-
-
-
-
-
-
-
-
-        }
-        public static void TriBule(List<int> _t)
-        {
-            bool flag = true;
-            int c = 0;
-            do
-            {
-                flag = true;
-                for (int i = 0; i < (_t.Count - 1); i++)
-
-                    if (_t[i] > _t[i + 1])
-                    {
-                        c = _t[i];
-                        _t[i] = _t[i + 1];
-                        _t[i + 1] = c;
-                        flag = false;
-                    }
-            } while (!flag);
-
-        }
     }
 }
