546a15ad45d7314bd1acffe742b2278da2e30930
tarik <73602347+ryfton@users.noreply.github.com>
2021-05-25 17:01:40 +0200

    pendule

---
+++ JeuxDuPendu/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JeuxDuPendu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int scoreJoueurDeux = 6;
+            string mot;
+            char recherche;
+            string motAffichage;
+            
+            
+            Console.WriteLine("veuillez saisir un mot de 5 caractères minimum: ");
+            mot = Console.ReadLine();
+            motAffichage = mot;
+            
+            if (mot.Count()<5)
+            {
+                Console.WriteLine("Le mot ne contient pas assez de caractères ! ");
+
+            }
+            else
+            {
+                //char[] motTrouver =mot.ToCharArray();
+                char[] motJoueurDeux = new char[4];
+                char[] motJoueurUn = mot.ToCharArray();
+                while (scoreJoueurDeux>0 && motJoueurDeux!=motJoueurUn)
+                {
+                    Console.WriteLine("Veuillez saisir une lettre pour trouver le mot :");
+                    string c = Console.ReadLine();
+                    char caratére = char.Parse(c);
+
+                     motJoueurDeux[caratére]
+
+                    recherche = RechercheCaractére(caratére,mot);
+                    if (recherche==caratére)
+                    {
+                        Console.WriteLine(recherche);
+                    }
+                    else
+                    {
+                        scoreJoueurDeux --;
+                    }
+
+
+
+
+                }
+                if (motJoueurDeux==motJoueurUn)
+                {
+                    Console.WriteLine("Bravo vous avez trouver le mot "+mot);
+                }
+                else
+                {
+                    Console.WriteLine("Désoler vous n'avez pas trouver le mot "+mot); 
+                }
+
+             
+            }
+            
+
+          
+            Console.ReadKey();
+
+       
+
+        }
+        public static char RechercheCaractére(char _a, string _chaineCaractére)
+        {
+            bool charOk=false;
+            char vide = '_';
+            foreach (char c in _chaineCaractére)
+            {
+                if (c == _a)
+                {
+                    charOk = true;
+                }
+
+            }
+            if (charOk==true)
+            {
+                return _a;
+            }
+            else
+            {
+                return vide;
+            }
+            
+        }
+    }
+}
--- TriDunTableau/Program.cs
+++ TriDunTableau/Program.cs
@@ -11,36 +11,65 @@ namespace Palindrom
     {
         static void Main(string[] args)
         {
-            string phrase,phraseReverse,finDePhrase;
-            bool palindrom=false;
-            
+
+            string phrase, finDePhrase;
+            bool palindrom = false;
+
             Console.WriteLine("veuillez saisir une phrase terminer par un point:");
             phrase = Console.ReadLine().ToLower();
             
+
             // teste de phrase vide.
-            finDePhrase=phrase.Substring(phrase.Length - 1);
-            if (finDePhrase == "."&& phrase.Length<1)
+            finDePhrase = phrase.Substring(phrase.Length - 1);
+            if (finDePhrase == "." && phrase.Length < 1)
             {
                 Console.WriteLine("la phrase est vide");
-            }else
-            { phrase = phrase.Substring(0,phrase.Length-1);
-                while (!palindrom)
-                {
+            }
+            else
 
-                    Console.WriteLine(phrase);
+            {
+                phrase = phrase.Substring(0, phrase.Length - 1);
+
+
+                char[] phraseChar = phrase.ToCharArray();
+
+                char[] phraseReverse = new char[phrase.Length];
+
+                for (int i = 0; i < phraseChar.Length; i++)
+                {   // ajouter -i a phrasechar.lenght
+                    phraseReverse[i] = phraseChar[(phraseChar.Length - 1) - i];
+                    //debug
+                    //Console.WriteLine(phraseReverse[i]);
+                }
+                for (int i = 0; i < phraseChar.Length; i++)
+                {
+                    if (phraseChar[i] != phraseReverse[i])
+                    {
+                        palindrom = false;
+                        //debug
+                        Console.Write(phraseReverse[i]);
+                    }
+                    else
+                    {//debug
+                        Console.Write(phraseReverse[i]);
+                        palindrom = true;
+                    }
 
                 }
-
-
+                if (palindrom == true)
+                {
+                    Console.WriteLine(" la phrase est un palindrome");
+                }
+                else
+                {
+                    Console.WriteLine(" la phrase est pas un palindrome");
+                }
             }
-
 
             Console.ReadLine();
 
 
         }
-            
 
-
+        }
     }
-}
