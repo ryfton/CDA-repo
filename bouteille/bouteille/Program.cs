de8c0556b4c536844c8724d944437ed12e67ad84
tarik <TChakir@arfp.asso.fr>
2021-06-03 12:13:24 +0200

    début bouteille

---
+++ bouteille/bouteille/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeBouteille
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal contenace;
+            decimal contenu;
+            bool ouvert;
+            decimal arajouter=10;
+            Bouteille perrierBouteille;
+            perrierBouteille = new Bouteille();
+            perrierBouteille.Ouvrir();
+            perrierBouteille.Remplir(arajouter);
+
+
+        }
+    }
+}
