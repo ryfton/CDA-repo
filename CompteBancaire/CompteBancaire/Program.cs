40b282ee65b30ced8fed963c7987d60e9d26f9d0
tarik <TChakir@arfp.asso.fr>
2021-06-21 09:19:39 +0200

    exercice Compte Bancaire

---
+++ CompteBancaire/CompteBancaire/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompteBancaire
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CompteBancaire c = new CompteBancaire(0001, "franck", 1500, -1000);
+            CompteBancaire c2 = new CompteBancaire(0002, "Collet", 25000, -500);
+            c.Transferer(10000, c2);
+            Console.WriteLine(c.ToString());
+            Console.WriteLine(c2.ToString());
+            if (c.SuperieurAutreCompte(c2))
+            {
+                Console.WriteLine("superieur");
+            }
+            else
+            {
+                Console.WriteLine("inférieure");
+            }
+
+        }
+    }
+}
---
+++ laVoiture/ClassLibraryPourLaVoitureTests/RoueTests.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibraryPourLaVoiture;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryPourLaVoiture.Tests
+{
+    [TestClass()]
+    public class RoueTests
+    {
+        [TestMethod()]
+        public void ConstructeurParDefaut_Voiture_NbCV()
+        {
+            Voiture cars = new Voiture();
+            Assert.AreEqual(cars.SonMoteur.NbCV,4);
+
+
+        }
+        [TestMethod()]
+        public void ConstructeurParDefaut_Voiture_Kilometrage()
+        {
+            Voiture cars = new Voiture();
+            Assert.AreEqual(cars.CompteurEnKM,0);
+
+
+        }
+        [TestMethod()]
+        public void ConstructeurParDefaut_Voiture_NbRoue()
+        {
+            Voiture cars = new Voiture();
+            Assert.AreEqual(cars.MesRoues.Count, 4);
+
+
+        }
+        [TestMethod()]
+        public void TesteMoteur_VoitureDemarer()
+        {
+            Voiture cars = new Voiture();
+            if (cars.Demarrer())
+            {
+                Assert.AreEqual(cars.SonMoteur.EstDemarre, true);
+            }
+            
+            
+
+        }
+    }
+}
