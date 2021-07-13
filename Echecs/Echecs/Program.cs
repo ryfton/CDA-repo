9153493bfbffba7a370d97b16d9e94025102194b
Gage <72741160+pascalgage@users.noreply.github.com>
2021-07-13 11:32:21 +0200

    Feature cases  (#13)
    * push
    
    * hello Tarik world...salut chef !!! (#11)
    
    Co-authored-by: Pascal GAGE <PGage@arfp.asso.fr>
    
    * frfr
    
    * position
    
    * teste
    
    * aj
    
    hgfd
    
    * PG ajout ligne test
    
    Co-authored-by: tarik <73602347+ryfton@users.noreply.github.com>
    Co-authored-by: Pascal GAGE <PGage@arfp.asso.fr>

---
+++ Echecs/Echecs/Program.cs
@@ -0,0 +1,32 @@
+﻿using JeuxEchecs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibraryechecs;
+
+namespace Echecs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StruCaseCharInt positionTourBlancheGauche = new StruCaseCharInt('a',1);
+            Tour tourBlancheGauche = new Tour("blanche","Tour", positionTourBlancheGauche);
+
+            //Début initialisation
+            StruCaseCharInt pca1 = new StruCaseCharInt('a', 1);
+            Case a1 = new Case(pca1, "noire");
+            StruCaseCharInt pcb1 = new StruCaseCharInt('b', 1);
+            Case b1 = new Case(pcb1, "blanche");
+            StruCaseCharInt pcc1 = new StruCaseCharInt('c', 1);
+            Case c1 = new Case(pcc1, "noire");
+            StruCaseCharInt pcd1 = new StruCaseCharInt('d', 1);
+            Case d1 = new Case(pcd1, "blanche");
+            int i = 0;
+
+
+        }
+    }
+}
