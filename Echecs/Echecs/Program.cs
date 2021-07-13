2d60e2b8078b18108b508e3d005be0e794bb63ac
Pascal GAGE <PGage@arfp.asso.fr>
2021-07-13 11:09:14 +0200

    aj
    hgfd

--- Echecs/Echecs/Program.cs
+++ Echecs/Echecs/Program.cs
@@ -14,10 +14,18 @@ namespace Echecs
         {
             StruCaseCharInt positionTourBlancheGauche = new StruCaseCharInt('a',1);
             Tour tourBlancheGauche = new Tour("blanche","Tour", positionTourBlancheGauche);
-            StruCaseCharInt pca1 = new StruCaseCharInt('a', 1);
-            Case a1 = new Case(pca1, "noir");
 
-            
+            //Début initialisation
+            StruCaseCharInt pca1 = new StruCaseCharInt('a', 1);
+            Case a1 = new Case(pca1, "noire");
+            StruCaseCharInt pcb1 = new StruCaseCharInt('b', 1);
+            Case b1 = new Case(pcb1, "blanche");
+            StruCaseCharInt pcc1 = new StruCaseCharInt('c', 1);
+            Case c1 = new Case(pcc1, "noire");
+            StruCaseCharInt pcd1 = new StruCaseCharInt('d', 1);
+            Case d1 = new Case(pcd1, "blanche");
+
+
         }
     }
 }
